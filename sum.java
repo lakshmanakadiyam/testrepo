f0859d7f4de2115c331873a108f8b092789669d9
lakshmanakadiyam <lakshmana1542@gmail.com>
2022-07-02 07:08:30 +0530

    java sum program

---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum 
+{
+int a, int b;
+public void static main(String args [])
+
+
+{
+
+int a=25, b=25,sum;
+
+sum =a+b;
+
+System.out.println("the sum of the number is" :+sum);
+}
+}
